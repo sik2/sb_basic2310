390c0e61fa20bc693bc03ab8ddd0b8c3e1a17070
sik2 <bickwang123@gmail.com>
2023-11-13 12:58:52 +0900

    컨트롤러 클래스에서 다양한 리턴타입을 가진 메소드, 롬복설명, @AllArgsConstructor, @Getter, @Setter

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -1,9 +1,17 @@
 package com.std.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
@@ -73,7 +81,167 @@ public class HomeController {
 
     @GetMapping("/home/divide")
     @ResponseBody
-    public int shoDivide(int a,  int b) {
+    public int showDivide(int a,  int b) {
         return a / b;
     }
+
+    @GetMapping("/home/returnBoolean")
+    @ResponseBody
+    public boolean returnBoolean () {
+        return true;
+    }
+
+    @GetMapping("/home/returnDouble")
+    @ResponseBody
+    public double returnDouble () {
+        return Math.PI;
+    }
+
+    @GetMapping("/home/returnIntArray")
+    @ResponseBody
+    public int[] showReturnIntArray() {
+        int[] arr = new int[]{10, 20, 30};
+
+        return arr;
+    }
+
+    @GetMapping("/home/returnIntList")
+    @ResponseBody
+    public List<Integer> showReturnIntList() {
+        List<Integer> list = new ArrayList<>() {{
+            add(10);
+            add(20);
+            add(30);
+        }};
+
+        return list;
+    }
+
+    @GetMapping("/home/returnMap")
+    @ResponseBody
+    public Map<String, Object> showReturnMap() {
+        Map<String, Object> map = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        return map;
+    }
+
+
+    @GetMapping("/home/returnCarV2")
+    @ResponseBody
+    public CarV2 showReturnCarV2() {
+        CarV2 car = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        car.setName(car.getName() + "V2");
+
+        return car;
+    }
+
+    @GetMapping("/home/returnCarMapList")
+    @ResponseBody
+    public List<Map<String, Object>> showReturnCarMapList() {
+        Map<String, Object> carMap1 = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        Map<String, Object> carMap2 = new LinkedHashMap<>() {{
+            put("id", 2);
+            put("speed", 200);
+            put("name", "포르쉐");
+            put("relatedIds", new ArrayList<>() {{
+                add(5);
+                add(6);
+                add(7);
+            }});
+        }};
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        list.add(carMap1);
+        list.add(carMap2);
+
+        return list;
+    }
+
+    @GetMapping("/home/returnCarList")
+    @ResponseBody
+    public List<CarV2> showReturnCarList() {
+        CarV2 car1 = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        CarV2 car2 = new CarV2(2, 200, "포르쉐", new ArrayList<>() {{
+            add(5);
+            add(6);
+            add(7);
+        }});
+
+        List<CarV2> list = new ArrayList<>();
+
+        list.add(car1);
+        list.add(car2);
+
+        return list;
+    }
+}
+
+class Car {
+    private final int id;
+    private final int speed;
+    private final String name;
+    private final List<Integer> relatedIds;
+
+    public Car(int id, int speed, String name, List<Integer> relatedIds) {
+        this.id = id;
+        this.speed = speed;
+        this.name = name;
+        this.relatedIds = relatedIds;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getRelatedIds() {
+        return relatedIds;
+    }
+}
+
+@Getter
+@AllArgsConstructor
+class CarV2 {
+    private final int id;
+    private final int speed;
+    @Setter
+    private String name;
+    private final List<Integer> relatedIds;
 }
